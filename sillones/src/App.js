fcac2dbf947037168d5373eb16eaab0ae7950f00
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-10-19 18:17:50 -0300

    Home

--- sillones/src/App.js
+++ sillones/src/App.js
@@ -7,14 +7,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<></>}></Route>
+          <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/shop" element={<></>}></Route>
           <Route exact path="/shop" element={<></>}></Route>
         </Routes>
         <Footer />
--- sillones/src/Components/Home/Home.js
+++ sillones/src/Components/Home/Home.js
@@ -2,6 +2,7 @@
 import "./styles.scss";
 //components
 import sillonFondo from "../../img/sillonFondo.jpg";
+import sillonHome from "../../img/sillonHome.jpg";
 import Maps from "../Maps/Maps";
 export const Home = () => {
   return (
@@ -10,21 +11,38 @@ export const Home = () => {
         <div className="home-header">
           <img src={sillonFondo} alt="" className="sillonFondo"></img>
           <div className="text-img">
-            <h1>Somos la empresa de los sillones</h1>
+            <h1>SILLONES DE DISEÑO</h1>
             <h2>¡Colchones a tu medida! Y mucho más..</h2>
           </div>
         </div>
-        <div className="home-footer">
-          <h4>homeo</h4>
-          <h3>SILLONES CARMEN - CONTÁCANOS</h3>
+        <div className="home-text">
+          <h4>Quiénes Somos</h4>
+          <h3>VENTA DE SILLONES DE DISEÑO</h3>
           <hr />
           <p>
             Dejanos tus datos y un representante se comunicará con vos para
             darte el mejor asesoramiento.
           </p>
         </div>
+        <div className="home-text-image">
+          <div>
+            <h1>Una tradición que se cumple desde hace mucho..</h1>
+            <h2>A LA TALLA DE TU HOGAR</h2>
+            <button type="button">Ver productos</button>
+          </div>
+
+          <div>
+            <img src={sillonHome} alt="" id="image1"></img>
+          </div>
+        </div>
+        <div className="home-images">
+          <img src={sillonHome} alt="" id="image1"></img>
+          <img src={sillonHome} alt="" id="image2"></img>
+          <img src={sillonHome} alt="" id="image3"></img>
+          <img src={sillonHome} alt="" id="image4"></img>
+          <img src={sillonHome} alt="" id="image5"></img>
+        </div>
       </div>
-      <Maps />
     </>
   );
 };
