5eb3679e33bd10462da250ebcafe147a8ad3f8d8
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-10-19 13:19:15 -0300

    footer

--- sillones/src/App.js
+++ sillones/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //components
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <>
@@ -13,8 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/services" element={<></>}></Route>
+          <Route exact path="/" element={<></>}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
