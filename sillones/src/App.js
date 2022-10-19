96ea6a6d5127343d2a76cacad02b2c3480ba2c1f
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-10-19 12:50:52 -0300

    finish home

--- sillones/src/App.js
+++ sillones/src/App.js
@@ -5,14 +5,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //components
 import Navbar from "./Components/Navbar/Navbar";
-
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<></>}></Route>
+          <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/services" element={<></>}></Route>
         </Routes>
       </BrowserRouter>
