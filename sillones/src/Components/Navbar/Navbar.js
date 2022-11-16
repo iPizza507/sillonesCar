8e9c76d48df6c73b73754027a6815521e91fadac
Ian Gonzalo Figueroa <ianfigueroa507@gmail.com>
2022-11-16 00:32:19 -0300

    Add photos and resize

--- sillones/src/Components/Navbar/Navbar.js
+++ sillones/src/Components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import "./styles.css";
 
 //
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import {
   AiOutlineInstagram,
   AiOutlineFacebook,
@@ -11,45 +12,58 @@ import {
 } from "react-icons/ai";
 
 export const Navbar = () => {
+  const [view, setView] = useState();
+
+  window.addEventListener("load", () => {
+    setView(window.innerWidth);
+  });
+  window.addEventListener("resize", () => {
+    setView(window.innerWidth);
+  });
+
   return (
     <>
-      <nav className="navbar" id="Navbar">
-        <div>
-          <Link to="/">IMAGEN</Link>
-        </div>
-        <div className="">
-          <ul>
-            <li>
-              <Link to="">Quienes Somos</Link>
-            </li>
-            <li>
-              <Link to="/product">Productos</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contacto</Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <a href="https://www.instagram.com/">
-                <AiOutlineInstagram />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/">
-                <AiOutlineFacebook />
-              </a>
-            </li>
-            <li>
-              <a href="https://web.whatsapp.com/">
-                <AiOutlineWhatsApp />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      {view > 767 ? (
+        <nav className="navbar" id="Navbar">
+          <div>
+            <Link to="/">IMAGEN</Link>
+          </div>
+          <div className="">
+            <ul>
+              <li>
+                <Link to="">Quienes Somos</Link>
+              </li>
+              <li>
+                <Link to="/product">Productos</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contacto</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/">
+                  <AiOutlineInstagram />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/">
+                  <AiOutlineFacebook />
+                </a>
+              </li>
+              <li>
+                <a href="https://web.whatsapp.com/">
+                  <AiOutlineWhatsApp />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      ) : (
+        <div>ALGO</div>
+      )}
     </>
   );
 };
--- sillones/src/Components/Product/Products.js
+++ sillones/src/Components/Product/Products.js
@@ -1,19 +1,37 @@
 const Sillones = [
   {
     id: 1,
-    url: require(`../../img/sillonFondo.jpg`),
+    url: require(`../../img/sillon1.jpg`),
     nombre: "Sillon de 4 cuerpos",
     textoInformativo: "Sillon gris con medidad tanto y material lindo",
   },
   {
     id: 2,
-    url: require(`../../img/sillonFondo1.jpg`),
+    url: require(`../../img/sillon2.jpg`),
     nombre: "Sillon de 1 cuerpo",
     textoInformativo: "Sillon verde con medidad tanto y material verde",
   },
   {
     id: 3,
-    url: require(`../../img/sillonFondo2.jpg`),
+    url: require(`../../img/sillon3.jpg`),
+    nombre: "Sillon Dúo",
+    textoInformativo: "Sillon blanco con medidad tanto y material..",
+  },
+  {
+    id: 4,
+    url: require(`../../img/sillon4.jpg`),
+    nombre: "Sillon Dúo",
+    textoInformativo: "Sillon blanco con medidad tanto y material..",
+  },
+  {
+    id: 5,
+    url: require(`../../img/sillon5.jpg`),
+    nombre: "Sillon Dúo",
+    textoInformativo: "Sillon blanco con medidad tanto y material..",
+  },
+  {
+    id: 6,
+    url: require(`../../img/sillon6.jpg`),
     nombre: "Sillon Dúo",
     textoInformativo: "Sillon blanco con medidad tanto y material..",
   },
